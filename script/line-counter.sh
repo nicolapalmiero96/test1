8f094307cd891c662796ed3d20eadb40d9dd385a
nicolapalmiero96 <nicolapalmiero96@gmail.com>
2024-01-31 12:54:34 +0100

    Fixed line-counter.sh

--- script/line-counter.sh
+++ script/line-counter.sh
@@ -1,3 +1,3 @@
 lines=$(cat *.cpp | wc -l)
-echo "La mia code base contiente": $lines"
-sed  -e "s/__LINES__/${lines}/" script/report-template.md > README.md
+echo $lines
+sed -e "s/__LINES__/${lines}/" script/report-template.md > README.md
