39956620ef58b87ef46f86d90e81d5b0bec1d6e8
nicolapalmiero96 <133234870+nicolapalmiero96@users.noreply.github.com>
2024-01-31 12:26:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello guys!" << std::endl;
     std::cout << "Hello guys!" << std::endl;
     std::cout << "Hello guys!" << std::endl;
+    std::cout << "Hello guys!" << std::endl;
 
     return 0;
 }
